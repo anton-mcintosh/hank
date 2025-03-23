c4e35a5fd1fde9d1744fdc95a36008e524b9eed2
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2025-03-23 09:21:59 -0700

    routes and tab bar established

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -2,11 +2,13 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { HapticTab } from '../components/HapticTab';
+import { IconSymbol } from '../components/ui/IconSymbol';
+import TabBarBackground from '../components/ui/TabBarBackground';
+import { Colors } from '../constants/Colors';
+import { useColorScheme } from '../hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -37,7 +39,28 @@ export default function TabLayout() {
         name="workorders"
         options={{
           title: 'Work Orders',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="clipboard.fill" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome5 name="clipboard-list" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="customers"
+        options={{
+          title: 'Customers',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="people-group" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="invoices"
+        options={{
+          title: 'Invoices',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="file-invoice-dollar" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="more"
+        options={{
+          title: 'More',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="hamburger" size={24} color={color} />
         }}
       />
     </Tabs>
